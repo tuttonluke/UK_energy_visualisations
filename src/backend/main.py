4314c7e6411a64102b6d2224901ddb3a2f75edb9
Luke Tutton <luke@rfcreations.com>
2026-06-06 13:29:58 +0100

    solar map: region highlighting and popups

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,7 @@
 import json
 import os
+import requests
+import time
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -15,12 +17,51 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Load static map data
 try:
     with open("gb-dno-license-areas-2024_wgs84.geojson", "r") as f:
         STATIC_GEOJSON_DATA = json.load(f)
 except FileNotFoundError:
     STATIC_GEOJSON_DATA = {"error": "regions_wgs84.geojson file not found."}
 
+# Setup memory cache for solar data
+SOLAR_CACHE = {}
+LAST_FETCH_TIME = 0
+CACHE_TTL = 300 # 5 minutes
+
+def get_live_solar_data():
+    global SOLAR_CACHE, LAST_FETCH_TIME
+
+    if time.time() - LAST_FETCH_TIME < CACHE_TTL and SOLAR_CACHE:
+        return SOLAR_CACHE
+    
+    print("Cache expired. Fetching fresh data from PV_Live API...")
+    new_data = {}
+
+    # GB's 14 PES regions are numbered 10-23
+    total_generation_mw = 0
+    for pes_id in range(10, 24):
+        url = f"https://api.pvlive.uk/pvlive/api/v4/pes/{pes_id}"
+
+        try:
+            response = requests.get(url, timeout=5).json()
+
+            if response.get("data") and len(response["data"]) > 0:
+                generation = response["data"][0][2]
+                new_data[str(pes_id)] = generation
+                total_generation_mw += generation
+        
+        except Exception as e:
+            print(f"Failed to fetch data for PES {pes_id}: {e}")
+    
+    new_data["total_gen"] = round(total_generation_mw, 1)
+    SOLAR_CACHE = new_data
+    LAST_FETCH_TIME = time.time()
+    print ("Live data cached successfully.")
+
+    return SOLAR_CACHE
+
+# API endpoints
 @app.get("/api/config")
 def get_config():
     token = os.getenv("MAPBOX_ACCESS_TOKEN")
@@ -29,6 +70,10 @@ def get_config():
 @app.get("/api/regions")
 def get_regions():
     return JSONResponse(content=STATIC_GEOJSON_DATA)
+
+@app.get("/api/solar")
+def get_solar():
+    return get_live_solar_data()
     
 if __name__ == "__main__":
     import uvicorn
--- src/frontend/app.js
+++ src/frontend/app.js
@@ -2,9 +2,9 @@ async function initialise_app() {
     try {
         const config_response = await fetch('http://127.0.0.1:8000/api/config');
         const config = await config_response.json();
-        
         mapboxgl.accessToken = config.mapboxToken;
 
+        // Map configuration
         const map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/dark-v11',
@@ -12,20 +12,133 @@ async function initialise_app() {
             zoom: 5
         });
 
-        map.on('load', async () => {
-            const regionsResponse = await fetch('http://127.0.0.1:8000/api/regions');
-            const geojsonData = await regionsResponse.json();
+        // Tooltip object
+        const popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false,
+            className: 'custom-popup'
+        });
 
-            map.addSource('pes-regions-source', { type: 'geojson', data: geojsonData });
+        let hovered_polygon_id = null;
+
+        map.on('load', async () => {
+            const regions_response = await fetch('http://127.0.0.1:8000/api/regions');
+            const geojsonData = await regions_response.json();
+
+            const solar_response = await fetch('http://127.0.0.1:8000/api/solar');
+            const solar_data = await solar_response.json();
+
+            geojsonData.features.forEach(feature => {
+                feature.id = feature.properties.ID;
+                const mw_value = solar_data[feature.id];
+                feature.properties.generation = (mw_value !== undefined && mw_value !== null) ? mw_value : 0;
+            });
+
+            // Add map source
+            map.addSource('pes-regions-source', { 
+                type: 'geojson', 
+                data: geojsonData 
+            });
+
+            // Map background colour
             map.addLayer({
-                id: 'pes-regions-layer',
+                id: 'pes-regions-fill',
                 type: 'fill',
                 source: 'pes-regions-source',
                 paint: {
-                    'fill-color': '#3498db',
-                    'fill-opacity': 0.5,
-                    'fill-outline-color': '#ffffff'
+                    'fill-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'generation'],
+                        0,   '#1e272c',
+                        50,
+                            '#f1c40f',
+                        200,
+                            '#e67e22',
+                        500,
+                            '#e74c3c'
+                    ],
+                    'fill-opacity': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        0.7, // opacity when hovered
+                        0.3  // opacity when not hovered
+                    ]
                 }
+            });
+
+            // Add region borders
+            map.addLayer({
+                id: 'pes-regions-borders',
+                type: 'line',
+                source: 'pes-regions-source',
+                paint: {
+                    'line-color': '#ffffff',
+                    'line-width': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        3,  // Thick border on hover
+                        0.5 // Thin border otherwise
+                    ]
+                }
+            });
+
+            // Mouse events
+            map.on('mousemove', 'pes-regions-fill', (e) => {
+                if (e.features.length > 0) {
+                    const current_feature = e.features[0];
+
+                    if (hovered_polygon_id != null) {
+                        map.setFeatureState(
+                            { source: 'pes-regions-source', id: hovered_polygon_id },
+                            { hover: false }
+                        );
+                    }
+                hovered_polygon_id = current_feature.id;
+                map.setFeatureState(
+                    { source: 'pes-regions-source', id: hovered_polygon_id },
+                    { hover: true }
+                );
+
+                // Tooltip logic
+                const region_name = current_feature.properties.Area;
+                const dnoName = current_feature.properties.DNO_Full;
+
+                const generation_mw = solar_data[current_feature.id];
+                const total_generation_mw = solar_data["total_gen"]
+                const display_total_data = (total_generation_mw !== undefined && total_generation_mw !== null)
+                                           ? `${total_generation_mw} MW` 
+                                           : "0 MW (Night/Offline)";
+                const display_region_data = (generation_mw !== undefined && generation_mw !== null)
+                                             ? `${generation_mw} MW` 
+                                             : "0 MW (Night/Offline)";
+
+                popup.setLngLat(e.lngLat)
+                    .setHTML(`
+                        <div style="font-family: sans-serif; padding: 5px;">
+                            <h3 style="margin: 0 0 5px 0;">${region_name}</h3>
+                            <p style="margin: 0; font-size: 12px; color: #666;">${dnoName}</p>
+                            <p style="margin: 0; font-size: 12px; color: #666;">PES ID: <span style="color: #666;">${current_feature.id}</span></p>
+                            <hr style="border: 0; border-top: 1px solid #ccc; margin: 8px 0;">
+                            <p style="margin: 0;">Total GB Solar Output: <span style="color: #e67e22;">${display_total_data}</span></p>
+                            <p style="margin: 0; font-weight: bold;">Region Solar Output: <span style="color: #e67e22;">${display_region_data}</span></p>
+                        </div>
+                    `)
+                    .addTo(map);
+                }
+            });
+
+            // When mouse leaves regions entirely
+            map.on('mouseleave', 'pes-regions-fill', () => {
+                if (hovered_polygon_id !== null) {
+                    map.setFeatureState(
+                        { source: 'pes-regions-source', id: hovered_polygon_id },
+                        { hover: false }
+                    );
+                }
+                hovered_polygon_id = null;
+            
+                popup.remove();
             });
         });
 
